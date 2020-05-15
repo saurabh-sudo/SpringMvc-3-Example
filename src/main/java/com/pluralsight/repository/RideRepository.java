daa4610cee850b1e4033feea5bf535b4fa4c3d4a
Saurabh <saurabh96mishra@gmail.com>
2020-05-15 16:17:06 +0530

    spring mvc3 Example

---
+++ src/main/java/com/pluralsight/repository/RideRepository.java
@@ -0,0 +1,11 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Ride;
+
+import java.util.List;
+
+public interface RideRepository {
+
+	List<Ride> getRides();
+
+}
---
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -0,0 +1,11 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Ride;
+
+import java.util.List;
+
+public interface RideService {
+
+	List<Ride> getRides();
+
+}
